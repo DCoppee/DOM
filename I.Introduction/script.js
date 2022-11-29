47d17de63f3ae98a8eaa0a3c2a8dca2cefb018ec
DCoppee <dominiquecoppee.pro@gmail.com>
2022-11-29 10:26:47 +0100

    Exercices Introduction

---
+++ I.Introduction/script.js
@@ -0,0 +1,6 @@
+//Change the background color of the body
+document.body.style.backgroundColor = "#FF69B4";
+//Display all children elements of the <body>
+for (elem of document.body.childNodes) {
+    console.log();
+}
